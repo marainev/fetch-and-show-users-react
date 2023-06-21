bac19c49c5178b0d652266f3d6bd862fc171fda1
marainev <mrvgllms@gmail.com>
2023-06-20 20:48:44 -0400

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   const [data, setData] = useState({
     // results: []
   });
+
+  const [users, setUsers] = useState([]);
+
+  const [searchData, setSearchData] = useState('');
   
   const {results} = data;
 
@@ -20,18 +24,41 @@ function App() {
       const rawData = await fetch("https://randomuser.me/api/?inc=name,picture&results=100");
       const data = await rawData.json();
       setData(data);
-    })()
-
+      setUsers(data.results || [])
+    })();
   }, []);
+
+  useEffect(() => {
+    const newUsers = results?.filter((user) => {
+
+      const fullName = `${user.name.title}${user.name.first}${user.name.last}`
+      if (
+        fullName.toLowerCase().replaceAll(' ','').includes(searchData)
+      ){
+        return true;
+      }
+      return false;
+    });
+    setUsers(newUsers || []);
+
+  }, [searchData]);
 
   return (
     <div id="app">
       <h1>List of users</h1>
       <div className="container">
+      <input 
+          id="filter" 
+          className="form-control mb-3 form-control-lg" 
+          placeholder="Type to filer..."
+          onChange={(event) => {
+            setSearchData(event.target.value.toLowerCase().replaceAll(' ',''))
+          }}
+        />
         <div className="users row">
 
           {
-            results?.map((item, index) => {
+            users.map((item, index) => {
 
               const finalName = `${item.name.title} ${item.name.first} ${item.name.last}`;
               return <div className="col-2 user" key={`item-${index}`}>
